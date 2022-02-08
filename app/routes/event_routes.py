58a6582208aba3b5503bdacd1d49dbcff22b9985
Alie Ibarra <alieibarra@gmail.com>
2022-02-08 11:48:45 -0800

    Add CRUD Routes for Tasks and Events

--- app/routes/event_routes.py
+++ app/routes/event_routes.py
@@ -6,11 +6,60 @@ from datetime import datetime
 
 event_bp = Blueprint("event", __name__, url_prefix="/events")
 
+# Helper Function
+def get_event_from_id(id):
+    try:
+        id = int(id)
+    except:
+        abort(400, {"error": "invalid id"})
+    return Event.query.get_or_404(id)
+
+#CREATE
+@event_bp.route("", methods=["POST"])
+def create_event():
+    request_body = request.get_json()
+    new_event = Event(
+        title=request_body["title"],
+        date=request_body["date"],
+    )
+
+    db.session.add(new_event)
+    db.session.commit()
+
+    return make_response(f"New task {new_event.title} successfully created!", 201)
+
+#READ
 @event_bp.route("", methods=["GET"])
-def get_all_events():
+def read_all_events():
     events = Event.query.all()
     
     events_response = []
     for event in events:
         events_response.append(event.to_dict())
     return jsonify(events_response)
+
+@event_bp.route("<id>", methods=["GET"])
+def read_one_event(id):
+    event = get_event_from_id(id)
+    return jsonify(event.to_dict())
+
+#UPDATE
+@event_bp.route("<id>", methods=["PUT"])
+def update_event(id):
+    event = get_event_from_id(id)
+    request_body = request.get_json()
+    if "title" in request_body:
+        event.title = request_body["title"]
+    if "date" in request_body:
+        event.date = request_body["date"]
+    
+    db.session.commit()
+    return jsonify([event.to_dict(), "Update Successful"])
+
+#DELETE
+@event_bp.route("<id>", methods=["DELETE"])
+def delete_event(id):
+    event = get_event_from_id(id)
+    db.session.delete(event)
+    db.session.commit()
+    return make_response("Delete successful", 200)
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -6,11 +6,61 @@ from datetime import datetime
 
 task_bp = Blueprint("task", __name__, url_prefix="/tasks")
 
+# Helper Function
+def get_task_from_id(id):
+    try:
+        id = int(id)
+    except:
+        abort(400, {"error": "invalid id"})
+    return Task.query.get_or_404(id)
 
+#CREATE
+@task_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task(
+        title=request_body["title"],
+        priority=request_body["priority"],
+        dueDate=request_body["dueDate"],
+    )
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(f"New task {new_task.title} successfully created!", 201)
+
+#READ
 @task_bp.route("", methods=["GET"])
-def get_all_tasks():
+def read_all_tasks():
     tasks = Task.query.all()
     tasks_response = []
     for task in tasks:
         tasks_response.append(task.to_dict())
     return jsonify(tasks_response)
+
+@task_bp.route("<id>", methods=["GET"])
+def read_one_tasks(id):
+    task = get_task_from_id(id)
+    return jsonify(task.to_dict())
+
+#UPDATE
+@task_bp.route("<id>", methods=["PUT"])
+def update_task(id):
+    task = get_task_from_id(id)
+    request_body = request.get_json()
+    if "title" in request_body:
+        task.title = request_body["title"]
+    if "dueDate" in request_body:
+        task.dueDate = request_body["dueDate"]
+    if "priority" in request_body:
+        task.priority = request_body["priority"]
+    db.session.commit()
+    return jsonify([task.to_dict(), "Update Successful"])
+
+#DELETE
+@task_bp.route("<id>", methods=["DELETE"])
+def delete_task(id):
+    task = get_task_from_id(id)
+    db.session.delete(task)
+    db.session.commit()
+    return make_response("Delete successful", 200)
