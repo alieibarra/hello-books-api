a66bb76564ea3b85ec537e75d96f02d92d51a629
Alie Ibarra <alieibarra@gmail.com>
2022-02-15 10:27:30 -0800

    Add Flask-Cors

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Backend for myDash React App
+Use this backend to support your creating, editing, and deleting of Tasks and Events from your myDash App.
 
--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -10,6 +11,7 @@ load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    CORS(app)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
